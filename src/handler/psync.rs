fe4d932eea608edee7d63ff06a83d40e8adda32e
zend <zendwang040302@outlook.com>
2024-05-24 09:31:38 +0800

    codecrafters submit [skip ci]

--- src/handler/psync.rs
+++ src/handler/psync.rs
@@ -19,7 +19,8 @@ pub async fn handle_psync(
         "Failed to respond when fullsync."
     );
 
-    let rdb = "";
+    let rdb_str = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+    
 
     stream.write(
         format!("${}\r\n{}", rdb.len(), rdb).as_bytes()
